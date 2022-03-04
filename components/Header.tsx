1717464d7c53246efd81448d7860e96c7f9e02ef
Cogentx <code@cogentx.com>
2022-03-04 18:10:30 -0330

    Header - Facebook logo using Next Image

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,15 @@ function Header() {
   return (
     <div className="flex justify-between m-2">
       {/* left */}
-      <div>Left</div>
+      <div className='flex items-center'>
+        <Image
+          src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
+          width={40}
+          height={40}
+          layout="fixed"
+          alt="facebook logo"
+        />
+      </div>
       {/* center */}
       <div>Center</div>
       {/* right */}
