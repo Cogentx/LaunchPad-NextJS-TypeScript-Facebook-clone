6c4d3ce2695c970424effcd62bfc250a145342af
Cogentx <code@cogentx.com>
2022-03-05 22:52:51 -0330

    Completed Header (Center)

--- components/HeaderIcon.tsx
+++ components/HeaderIcon.tsx
@@ -1,11 +1,12 @@
 interface Props {
   Icon: any;
+  active?: boolean;
 }
 
-function HeaderIcon({ Icon }: Props) {
+function HeaderIcon({ Icon, active }: Props) {
   return (
     <div className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl hover:text-blue-500 active:border-b-2 active:border-blue-500 group">
-      <Icon className="h-5 group-hover:text-blue-500 text-center sm:h-7 mx-auto" />
+      <Icon className={`h-5 text-gray-500 group-hover:text-blue-500 text-center sm:h-7 mx-auto ${active && 'text-blue-500'}`} />
     </div>
   );
 }
