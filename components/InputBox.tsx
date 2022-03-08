a561f9c55c9fb0e3a3279f944bed939776477917
Cogentx <code@cogentx.com>
2022-03-07 21:30:14 -0330

    InputBox Component (1/2) - with sendPost trigger

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -4,9 +4,13 @@ import { useSession } from 'next-auth/react';
 export default function InputBox() {
   const { data: session } = useSession();
 
+  const sendPost = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className='bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6'>
-      <div className='flex items-center space-x-4 p-4'>
+    <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+      <div className="flex items-center space-x-4 p-4">
         {session && session.user && (
           <Image
             className="rounded-full"
@@ -17,10 +21,15 @@ export default function InputBox() {
             alt="user profile picture"
           />
         )}
-        <form className='flex flex-1'>
-          <input type="text"
-          placeholder={`What's on your mind, ${session?.user?.name!}?`}
-          className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"/>
+        <form className="flex flex-1">
+          <input
+            type="text"
+            placeholder={`What's on your mind, ${session?.user?.name!}?`}
+            className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+          />
+          <button onClick={sendPost} hidden type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
