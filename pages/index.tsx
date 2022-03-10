7d98d1cfef1b05efaf26c959112cdb5f9e49091d
Cogentx <code@cogentx.com>
2022-03-10 16:28:19 -0330

    Server-side Render (SSR) pre-fetch Firebase data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 import type { NextPage, NextPageContext } from 'next';
 import { getSession, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Feed from '../components/Feed';
 import Header from '../components/Header';
 import Login from '../components/Login';
+import Posts from '../components/Posts';
 import Sidebar from '../components/Sidebar';
 import Widgets from '../components/Widgets';
+import { db, fb_posts_url } from '../firebase';
 
-export default function Home() {
+export default function Home({ posts }: any) {
   const { data: session, status } = useSession();
   if (!session) return <Login />;
+
+  console.log(posts);
 
   return (
     <div className="bg-gray-100 overflow-hidden h-screen">
@@ -24,16 +29,26 @@ export default function Home() {
       <main className="flex">
         <Sidebar />
         <Feed />
-        <Widgets/>
+        <Widgets />
       </main>
     </div>
   );
 }
 
 export async function getServerSideProps(context: NextPageContext) {
+  const posts = await getDocs(query(collection(db, fb_posts_url), orderBy('timestamp', 'desc')));
+
+  // cannot pre-fetch Firebase timestamp without losing critical information so set to null and get later
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null,
+  }));
+
   return {
     props: {
       session: await getSession(context),
+      posts: docs,
     },
   };
 }
