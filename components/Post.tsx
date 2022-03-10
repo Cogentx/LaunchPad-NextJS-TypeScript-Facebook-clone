1eb9cb87ae9280ee7d106a63dd4bf42c38491463
Cogentx <code@cogentx.com>
2022-03-10 13:32:10 -0330

    Post - footer - refactor styling and icons

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { ThumbUpIcon, AnnotationIcon, ShareIcon } from '@heroicons/react/outline';
+import { ThumbUpIcon, ChatAltIcon, ShareIcon } from '@heroicons/react/outline';
 import { IPost } from '../fb-clone';
 
 interface IProps {
@@ -10,36 +10,35 @@ export default function Post({ post }: IProps) {
   const { name, email, message, timestamp, image, postImage } = post;
 
   return (
-    <div className="flex flex-col">
-      <div className="bg-white p-5 mt-5 rounded-t-2xl shadow-md">
-        <div className="flex space-x-2 items-center">
-          <Image src={image!} className="rounded-full" layout="fixed" alt="profile picture" height={40} width={40} />
-          <div className="flex flex-col">
-            <p className="font-medium">{name}</p>
-            <p className="text-xs">{new Date(timestamp?.toDate()).toLocaleString()}</p>
-          </div>
+    <div className="flex flex-col bg-white pt-5 px-5 my-5 rounded-t-2xl shadow-md border-t rounded-b-2xl">
+      <div className="flex space-x-2 items-center">
+        <Image src={image!} className="rounded-full" layout="fixed" alt="profile picture" height={40} width={40} />
+        <div className="flex flex-col">
+          <p className="font-medium">{name}</p>
+          <p className="text-xs">{new Date(timestamp?.toDate()).toLocaleString()}</p>
         </div>
-        <div className="pt-4">
-          <p>{message}</p>
+      </div>
+      <div className="pt-4">
+        <p>{message}</p>
+      </div>
+      {postImage && (
+        <div className="relative h-56 md:h-96">
+          <Image src={postImage} className="" alt="Post Image" layout="fill" objectFit="cover" />
         </div>
-        {postImage && (
-          <div className='relative h-56 md:h-96'>
-            <Image src={postImage} className="" alt="Post Image" layout="fill" objectFit='cover'/>
-          </div>
-        )}
-        <div className="flex m-2 text-gray-500 ">
-          <div className="inputIcon">
-            <ThumbUpIcon className="h-5 w-5" />
-            <p className="text-xs sm:text-sm xl:text-base">Like</p>
-          </div>
-          <div className="inputIcon">
-            <AnnotationIcon className="h-5 w-5" />
-            <p className="text-xs sm:text-sm xl:text-base">Comment</p>
-          </div>
-          <div className="inputIcon">
-            <ShareIcon className="h-5 w-5" />
-            <p className="text-xs sm:text-sm xl:text-base">Share</p>
-          </div>
+      )}
+      {/* post footer */}
+      <div className="flex justify-between items-center text-gray-400">
+        <div className="inputIcon">
+          <ThumbUpIcon className="h-4 w-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon">
+          <ChatAltIcon className="h-4 w-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon">
+          <ShareIcon className="h-4 w-4" />
+          <p className="text-xs sm:text-base">Share</p>
         </div>
       </div>
     </div>
