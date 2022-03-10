960bc1570bb8227db751a3f3ac6cb37ed6eceaa7
Cogentx <code@cogentx.com>
2022-03-10 09:54:25 -0330

    Create Posts | Post Components

---
+++ components/Post.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Post(props:any) {
+
+  return <div>Post</div>;
+}
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,11 +1,15 @@
-import { collection, query } from 'firebase/firestore';
+import { collection, orderBy, query } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db, fb_posts_url } from '../firebase';
+import { IPost } from '../fb-clone';
+import Post from './Post';
 
 export default function Posts() {
-  const q = query(collection(db, fb_posts_url));
-  const [realtimePosts, loading, error] = useCollection()
+  const q = query(collection(db, fb_posts_url), orderBy('timestamp', 'desc'));
 
+  const [realtimePosts, loading, error] = useCollection(q);
 
-  return <div>Posts</div>;
+  return <div>
+    {realtimePosts?.docs.map((doc,index)=><Post key={index} post={doc.data()}/>)}
+  </div>;
 }
