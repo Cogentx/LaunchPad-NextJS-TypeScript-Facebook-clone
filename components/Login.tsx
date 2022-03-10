9512d988e10d50e60ad34279cfdbf3668f15386e
Cogentx <code@cogentx.com>
2022-03-10 09:39:44 -0330

    Customize Tailwind Color on Login Button

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,21 +1,25 @@
 import Image from 'next/image';
-import { signIn } from "next-auth/react";
+import { signIn } from 'next-auth/react';
 
 function Login() {
   return (
     <div className="grid place-items-center mt-10">
-        <Image
-          src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
-          width={400}
-          height={400}
-          objectFit="contain"
-          alt="facebook logo"
-          // use 'priority' to disable 'lazy-loading' on images 'above-the-fold'
-          priority
-        />
-        <button onClick={()=>signIn()} className="p-5 mt-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">
-          Login with Facebook
-        </button>
+      <Image
+        src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
+        width={400}
+        height={400}
+        objectFit="contain"
+        alt="facebook logo"
+        // use 'priority' to disable 'lazy-loading' on images 'above-the-fold'
+        priority
+      />
+      {/* Get color value using color picker on fb image for example. Remember '#' in front of hex-color value */}
+      <button
+        onClick={() => signIn()}
+        className="px-5 py-3 mt-5 bg-[#186ae7] rounded-full text-white text-center cursor-pointer"
+      >
+        Login with Facebook
+      </button>
     </div>
   );
 }
